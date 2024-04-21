8d74aa0db0517a9fa3989b060ea43a3cc9626e37
Matheus-bm <matheus.barreto.med@gmail.com>
2024-04-21 16:00:51 -0300

    list network devices

--- views/home.py
+++ views/home.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
-
+from utils import network
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
 def index():
+    device_list = network.list_devices("127.0.0.1/24")
+    print(device_list)
     return render_template("index.html")
