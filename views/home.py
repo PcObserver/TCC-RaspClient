f1ec09bc1ae0d80bfbcae7cbf91faf70bb87acd7
Matheus Barreto Medeiros <matheus.barreto.med@gmail.com>
2024-04-23 10:43:09 -0300

    index a and setup page layout

--- views/home.py
+++ views/home.py
@@ -6,3 +6,8 @@ main_blueprint = Blueprint("main", __name__)
 @main_blueprint.route("/")
 def index():
     return render_template("index.html")
+
+
+@main_blueprint.route("/setup")
+def setup():
+    return render_template("setup.html")
