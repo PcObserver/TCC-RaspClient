24b8ee705012a44e373833f98734ec46a69f95ef
Matheusbm <matheusmbm13@gmail.com>
2024-05-03 23:49:18 -0300

    update

--- models/device.py
+++ models/device.py
@@ -1,3 +1,4 @@
+from sqlalchemy import desc
 from sqlalchemy.sql import func
 from sqlalchemy.dialects.postgresql import UUID
 from application import db
@@ -8,5 +9,7 @@ class Device(db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     brand_id = db.Column(UUID(as_uuid=True), db.ForeignKey("brands.id"), nullable=False)
+    name = db.Column(db.String(25), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.TIMESTAMP, server_default=func.now())
     updated_at = db.Column(db.TIMESTAMP, server_default=func.now(), onupdate=func.now())
--- views/device.py
+++ views/device.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from multiprocessing import context
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.device import Device
 from models.brand import Brand
+from application import db
 
 device_blueprint = Blueprint("device", __name__)
 
@@ -10,6 +12,27 @@ def list_registered_devices():
     return render_template("device/index.html", devices=devices)
 
 
-@device_blueprint.route("/device/register")
-def register_device():
-    return render_template("device/register.html")
+@device_blueprint.route("/device/register", methods=["GET", "POST"])
+def create_device():
+    if request.method == "GET":
+        context = {
+            "brand": None #Brand.query.get(request.args.get('brand_id'))
+        }
+        return render_template("device/register.html", **context)
+    elif request.method == "POST":
+        name = request.form.get("name")
+        brand_id = request.form.get("brand_id")
+        device = Device(name=name, brand_id=brand_id)
+        db.session.add(device)
+        db.session.commit()
+        return redirect(url_for("device.list_devices_by_brand", brand_id=brand_id, selected_device=device.id))
+
+
+@device_blueprint.route("/brand/devices")
+def list_devices_by_brand():
+    context = {
+        "devices": [], #Device.query.filter_by(brand_id=request.args.get('brand_id')).all()
+        "brand_id": request.args.get('brand_id')
+    }
+
+    return render_template("device/devices.html", **context)
