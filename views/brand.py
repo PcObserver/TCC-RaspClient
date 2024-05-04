0db08f0667e73db3f367719ea6ebefe5729dfdde
Matheusbm <matheusmbm13@gmail.com>
2024-05-03 21:48:00 -0300

    draft brands create form

--- views/brand.py
+++ views/brand.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.brand import Brand
+from sqlalchemy.orm import Session 
 
 brand_blueprint = Blueprint("brand", __name__)
 
@@ -7,3 +8,17 @@ brand_blueprint = Blueprint("brand", __name__)
 def list_brands():
     brands =  Brand.query.all()
     return render_template("brand/index.html", brands=brands)
+
+@brand_blueprint.route("/device/brand", methods=["GET", "POST"])
+def create_brand():
+    if request.method == "GET":
+        return render_template("brand/new.html")
+    
+    elif request.method == "POST":
+        with Session() as session:
+            name = request.form.get("name")
+            prefix = request.form.get("prefix")
+            brand = Brand(name= name, prefix= prefix)
+            session.add(brand)
+            session.commit()
+        return redirect(url_for("brand.list_brands", selected_brand=brand.id))
