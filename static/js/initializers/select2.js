9d7fd1a6f0999663ee81c6501720120192bd92cc
Matheusbm <matheusmbm13@gmail.com>
2024-05-03 21:09:50 -0300

    draft select2-brands

--- static/js/initializers/select2.js
+++ static/js/initializers/select2.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    $('.select2').select2();
+    $('.select2-brands').select2({
+        ajax: {
+            url: '/api/brands',
+            dataType: 'json',
+            delay: 250,
+            data: function(params) {
+                return {
+                    search: params.term,
+                    page: params.page
+                };
+            },
+            processResults: function(data, params) {
+                return {
+                    results: data.data,
+                    pagination: {
+                        more: data.meta.current_page < data.meta.last_page
+                    }
+                };
+            },
+            cache: true
+        },
+    });
 });
